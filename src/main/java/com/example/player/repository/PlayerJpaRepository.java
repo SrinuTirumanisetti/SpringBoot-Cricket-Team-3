109186e05a40c9c166ad42c5427c00763be2097f
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-07-27 00:12:56 +0000

    Creating Repository Interface for ORM

--- src/main/java/com/example/player/repository/PlayerJpaRepository.java
+++ src/main/java/com/example/player/repository/PlayerJpaRepository.java
@@ -7,3 +7,12 @@
  */
 
 // Write your code here
+package com.example.player.repository;
+
+import com.example.player.model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerJpaRepository extends JpaRepository<Player, Integer> {
+}
