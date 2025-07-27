965522a78e2381f2c1ef64b66a3c3fd977b1ebdd
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-07-27 00:24:58 +0000

    Created instance of JPARepository

--- src/main/java/com/example/player/repository/PlayerRepository.java
+++ src/main/java/com/example/player/repository/PlayerRepository.java
@@ -1,1 +1,7 @@
 // Write your code here
+
+package com.example.player.repository;
+
+import com.example.player.model.Player;
+import java.util.*;
+
--- src/main/java/com/example/player/service/PlayerJpaService.java
+++ src/main/java/com/example/player/service/PlayerJpaService.java
@@ -10,3 +10,23 @@
  */
 
 // Write your code here
+
+package com.example.player.service;
+
+import com.example.player.model.Player;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.*;
+import com.example.player.repository.PlayerRepository;
+import com.example.player.repository.PlayerJpaRepository;
+
+@Service
+public class PlayerJpaService implements PlayerRepository{
+
+    @Autowired
+    public PlayerJpaRepository playerJpaRepository;
+
+    
+}
